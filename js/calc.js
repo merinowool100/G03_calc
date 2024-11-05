0e69667d21e51b6acb48daf08af5ff501c35de1f
merinowool100 <masatinib@gmail.com>
2024-11-06 01:38:00 +0900

    updated files

--- js/calc.js
+++ js/calc.js
@@ -1,8 +1,8 @@
 // 問題を格納するリスト
 const problems = [];
 
-// 二桁の足し算と引き算の問題をランダムに10問作成
-for (let i = 0; i < 10; i++) {
+// 二桁の足し算と引き算の問題をランダムに2問作成
+for (let i = 0; i < 2; i++) {
     let num1, num2, isAddition;
     do {
         num1 = Math.floor(Math.random() * 90) + 10; // 10から99の範囲
@@ -11,9 +11,9 @@ for (let i = 0; i < 10; i++) {
     } while ((isAddition && num1 + num2 > 100) || (!isAddition && num1 < num2));
 
     if (isAddition) {
-        problems.push({ question: `${num1} + ${num2}`, answer: num1 + num2 });
+        problems.push({ question: `${num1} + ${num2}`, answer: num1 + num2, mother: num1 });
     } else {
-        problems.push({ question: `${num1} - ${num2}`, answer: num1 - num2 });
+        problems.push({ question: `${num1} - ${num2}`, answer: num1 - num2, mother: num1 });
     }
 }
 
@@ -30,6 +30,63 @@ function showProblem() {
     const currentProblem = problems[currentProblemIndex];
     document.getElementById("problemDisplay").innerText = `Q: ${currentProblem.question} =`;
     updateRemainingDisplay(); // 残りの問題数を更新
+
+    // Canvasの設定--------------------------------------------
+    const canvas = document.querySelector(".canvas");
+    canvas.width = 320;
+    canvas.height = 320;
+    const context = canvas.getContext("2d");
+
+    // マス目描画用に数字を設定---------------------------------------
+    const rows = Math.floor(currentProblem.mother / 10);
+    const residual = currentProblem.mother % 10;
+
+    // 四角を描画する---------------------------------------------
+    context.fillStyle = "rgba(255,150,0,0.5)";
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(0, 320);
+    context.lineTo(rows * 32, 320);
+    context.lineTo(rows * 32, 0);
+    context.closePath();
+    context.fill();
+
+    // あまりの四角を描画する
+    context.fillStyle = "rgba(255,150,0,0.5)";
+    context.beginPath();
+    context.moveTo(rows * 32, 0);
+    context.lineTo(rows * 32, 32 * residual);
+    context.lineTo((rows + 1) * 32, 32 * residual);
+    context.lineTo((rows + 1) * 32, 0);
+    context.closePath();
+    context.fill();
+
+    // マス目を描画---------------------------------------------
+    function cells() {
+        const gridSize = 32;
+        const gridCount = 10;
+        for (let i = 0; i <= gridCount; i++) {
+            context.strokeStyle = "gray";
+            context.beginPath();
+            context.moveTo(0, i * gridSize); // 横線
+            context.lineTo(320, i * gridSize);
+            context.moveTo(i * gridSize, 0); // 縦線
+            context.lineTo(i * gridSize, 320);
+            context.closePath();
+            context.stroke();
+        }
+    };
+    cells();
+
+    context.strokeStyle = "black";
+    context.beginPath();
+    context.lineWidth = 3;
+    context.moveTo(0, 160);
+    context.lineTo(320, 160);
+    context.moveTo(160, 0);
+    context.lineTo(160, 320);
+    context.closePath();
+    context.stroke();
 }
 
 // 数字ボタンで入力を追加する関数
@@ -47,7 +104,7 @@ document.getElementById("clearButton").addEventListener("click", () => {
 function showFeedback(isCorrect) {
     const feedback = document.getElementById("feedback");
     const currentProblem = problems[currentProblemIndex];
-    
+
     if (isCorrect) {
         feedback.innerText = "〇"; // 正解のフィードバック
         feedback.style.color = "green";
@@ -57,7 +114,7 @@ function showFeedback(isCorrect) {
         // 正しい答えもフィードバックに表示
         feedback.innerText += `${currentProblem.answer}`;
     }
-    
+
     feedback.style.display = "block";
     feedback.classList.remove("hidden");
 
@@ -69,11 +126,16 @@ function showFeedback(isCorrect) {
             if (currentProblemIndex < problems.length) {
                 showProblem(); // 次の問題を表示
             } else {
-                document.getElementById("problemDisplay").innerText = "すべての問題が終了しました！";
+                document.getElementById("remainingDisplay").innerText = "残りの問題数: 0";
+                document.getElementById("problemDisplay").remove();
+                document.getElementById("answerArea").remove();
+                const completeMessage = document.createElement("div");
+                completeMessage.innerText = "Mission complete!!";
+                document.getElementById("problemDisplayContainer").appendChild(completeMessage);
                 document.getElementById("submitAnswerButton").disabled = true;
             }
-        }, 1000); // フェードアウト後に完全非表示
-    }, 1000); // 1秒間「〇」または「×」を表示してからフェードアウト開始
+        }, 300); // フェードアウト後に完全非表示
+    }, 300); // 0.3秒間「〇」または「×」を表示してからフェードアウト開始
 }
 
 // 回答をチェックする関数
